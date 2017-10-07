a4b354abba5304ad4fe2a931431afe522ab56540
Sebastian Kluge <SebastianKluge@gmx.de>
2017-10-07 19:53:16 +0000

    Finalizes parameters and debug output

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ int main()
 {
   uWS::Hub h;
 
-  PID pid(0.3, 0.0001, 1.0);
+  PID pid(0.22, 0.00009, 3.1, false);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -60,10 +60,6 @@ int main()
           if ( steer_value < -1.0 ) steer_value = -1.0;
           if ( steer_value >  1.0 ) steer_value =  1.0;
 
-          // calculate speed control
-          double throttle(0.3);
-          if ( speed > 20.0 ) throttle = 0.0;
-          
           // DEBUG
           //std::cout << "CTE: " << cte 
           //          << " Speed: " << speed
@@ -72,7 +68,7 @@ int main()
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"]       = throttle;
+          msgJson["throttle"]       = 0.3;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           //std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
